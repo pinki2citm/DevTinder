c0f6ff0ee2f76b5716e80acee9c2e195dd6adde6
Pinki Kumari <pinkikumaristm9@gmail.com>
2024-09-22 23:09:31 +0530

    hello routing

--- src/app.js
+++ src/app.js
@@ -5,6 +5,10 @@ const app= express();
 app.use("/pk",(req,res)=>{
     res.send("Hello DevTinder!!!");
 });
+app.use("/hello",(req,res)=>{
+    res.send("Hello DevTinder this is hello command!!!");
+});
+
 app.listen(3000,()=>{
     console.log("server created successfullly.!!");
 });

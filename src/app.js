8e1d7a444a54ceaeb2464bde749717bf3a63590b
Pinki Kumari <pinkikumaristm9@gmail.com>
2025-02-22 14:51:14 +0530

    Data Sanitization

--- src/app.js
+++ src/app.js
@@ -15,9 +15,8 @@ connectDB().then(()=>{
 
 // Middleware to parse JSON requests
 app.use(express.json());
-app.use(express.urlencoded({ extended: true })); // For form data
-app.use(cors(
-     {origin: 'http://localhost:5173',credentials:true})); 
+app.use(express.urlencoded({ extended: true })); 
+app.use(cors({origin: 'http://localhost:5173',credentials:true})); 
 
 //login API
 app.post("/login", (req, res) => {
@@ -39,6 +38,70 @@ app.post("/signup", async (req,res)=>{
   await  newUser.save();
     res.send("user added successfully!!!");
 });
+
+app.get ("/user",async (req,res)=>{
+const userEmail = req.body.email;
+try{
+    const user = await User.find({email: userEmail});
+    res.send(user);
+}
+catch(error)
+{
+    res.status(404).send("USer not found")
+}
+});
+
+app.get ("/feed",async(req,res)=>{
+    try{
+        const user = await User.find({});
+        res.send(user);
+    }
+    catch(error)
+    {
+        res.status(404).send("USer not found")
+    }
+});
+
+app.delete("/user", async (req,res)=>{
+const userId= req.body.userId
+try{
+    const user = await User.findByIdAndDelete(userId);
+    res.send("user deleted successfully!!")
+}
+catch(error)
+{
+    res.status(404).send("User not found")
+}
+})
+
+app.patch("/user",async(req,res)=>{
+
+    //  const userId = req.body.userId;
+    const userEmail = req.body.email;
+      const data = req.body;    
+          
+      try{
+          const ALLOWED_UPDATES = ["userId","photoURL", "about","gender","age","skills" ];
+          const isUpdateAllowed = Object.keys(data).every((k)=>
+              ALLOWED_UPDATES.includes(k)
+          );
+          if(!isUpdateAllowed)
+             { throw new Error("update not allowed");}
+  
+          if( data.skills.length>10)
+            {
+                throw new error("skills more than 10 are not allowed")
+            }
+
+          const user = await User.findOneAndUpdate({email:userEmail
+          }, data, { runValidators: true, new: true } );
+          res.send("user updated successfully!!")
+      }
+      catch(error)
+      {
+          res.status(404).send("Update failed " + error.message)
+      }
+  })
 app.use("/",(req,res)=>{
 console.log("Hi DevTinder");
 })
--- src/middlewares/models/user.js
+++ src/middlewares/models/user.js
@@ -1,26 +1,57 @@
 const mongoose = require('mongoose');
-
+const validator =require('validator');
 const UserSchema = new mongoose.Schema({
 
     firstName:{
         type: String,
+        required: true
     },
     lastName: {
         type: String,
     },
     email: {
         type: String,
+        validate(value)
+        {
+            if(!validator.isEmail(value)) 
+                throw new Error("invalid Email");
+            
+        }
+        required: true,
+        unique:true
     },
     password: {
         type: String,
+        required: true
     },
    age: {
         type: Number,
+        min: 18
     },
     gender: {
         type: String,
+        validate(value){
+            if(!["male","female","others"].includes(value))
+                throw new Error("Gender is not valid");
+        }
     },
-});
+    photoURL: {
+        type:String,
+        default: "https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png"
+    },
+    skills:{
+      type: [String],
+    },
+    about:{
+        type: String,
+        default: "This is the about text"
+    },
+   
+}, 
+{
+    timestamps:true,
+}
+);
 
 const User = mongoose.model("User",UserSchema);
 module.exports = User;
